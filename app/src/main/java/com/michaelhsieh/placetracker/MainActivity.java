53e639f9ee54f4ed9d84608b284aba3cbdbaca81
Michael Hsieh <mdhsieh8@gmail.com>
2020-06-22 16:35:45 -0700

    feat: Change theme colors and launcher icon.
    Changed color to orange, dark orange, and accent of blue.
    Replaced default app launcher icon with orange list icon.
    Display a Toast if not connected to Wi-Fi or cellular network.
    
    Runs on API 28 emulator and API 17 tablet.

--- app/src/main/java/com/michaelhsieh/placetracker/MainActivity.java
+++ app/src/main/java/com/michaelhsieh/placetracker/MainActivity.java
@@ -2,8 +2,15 @@ package com.michaelhsieh.placetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
@@ -23,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // show a Toast if there's no Internet connection (Wi-Fi or cellular network)
+        if (!isNetworkConnected()) {
+            Toast.makeText(this, R.string.internet_connection_error, Toast.LENGTH_LONG).show();
+        }
+
         // Initialize the SDK
         Places.initialize(getApplicationContext(), getString(R.string.google_places_api_key));
 
@@ -30,11 +42,11 @@ public class MainActivity extends AppCompatActivity {
         PlacesClient placesClient = Places.createClient(this);
 
         // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+        final AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
 
         // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS));
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
@@ -42,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
             public void onPlaceSelected(Place place) {
                 // TODO: Get info about the selected place.
                 Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                Log.i(TAG, "Place address: " + place.getAddress());
             }
 
             @Override
@@ -52,4 +65,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    /* Check if connected to Wi-Fi or cellular network.
+
+        Source:
+        pavelnazimok
+        https://stackoverflow.com/questions/32547006/connectivitymanager-getnetworkinfoint-deprecated
+     */
+    private boolean isNetworkConnected() {
+        final ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+            if (Build.VERSION.SDK_INT < 23) {
+                // use deprecated methods on older devices
+                final NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+                if (networkInfo != null) {
+                    return (networkInfo.isConnected() && (networkInfo.getType() == ConnectivityManager.TYPE_WIFI || networkInfo.getType() == ConnectivityManager.TYPE_MOBILE));
+                }
+            } else {
+                // getActiveNetwork() was added in API 23
+                final Network network = connectivityManager.getActiveNetwork();
+
+                if (network != null) {
+                    final NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+
+                    if (networkCapabilities != null) {
+                        return (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI));
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /*
+    Source:
+    Alex Jasmin
+    https://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
+     */
+    /*private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        } else {
+            Log.e(TAG, "ConnectivityManager is null");
+            return false;
+        }
+    }*/
 }
